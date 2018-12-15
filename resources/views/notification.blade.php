05e659f83fcd941999d3cfaafe1f334b5c2dd317
pritam <pb457@njit.edu>
2018-12-15 14:52:32 -0500

    Task:Email Notification view, controller created and routing done

---
+++ resources/views/notification.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1 style="color: black" style="background-color: #6cb2eb">Hello, You are being notified.
+        Please check your email !</h1>
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ Route::post('/questions/{question_id}/answers/', 'AnswerController@store')->name
 Route::patch('/questions/{question_id}/answer/{answer_id}', 'AnswerController@update')->name('answers.update');
 Route::delete('/questions/{question_id}/answer/{answer_id}', 'AnswerController@destroy')->name('answers.destroy');
 
+Route::get('/notification', 'ENotificationController@show')->name('notification.show');
 
 Route::resources([
     'questions' => 'QuestionController',
