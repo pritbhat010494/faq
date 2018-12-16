94a522b50c83b3d9c452669068e05338542faf93
pritam <pb457@njit.edu>
2018-12-16 13:02:34 -0500

    Dusk Test for Editing Question

---
+++ tests/Browser/EditQuestionTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class EditQuestionTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('http://localhost:8000')
+                ->assertTitle('Laravel')
+                ->clickLink('Login')
+                ->type('#email', 'bhatkarpritam@gmail.com')
+                ->type('#password', '123456')
+                ->press('button[type="submit"]')
+                ->assertSee('Questions')
+                ->clickLink('View')
+                ->assertSee('Question')
+                ->clickLink('Edit Question')
+                ->assertSee('Body')
+                ->type('#body', 'How was the party?')
+                ->click('#submit')
+                ->assertSee('Hello, You are being notified. Please check your email !
+Thank You!')
+                ->press('#navbarDropdown')
+                ->clickLink('Logout')
+                ->assertTitle('Laravel');
+        });
+    }
+}
